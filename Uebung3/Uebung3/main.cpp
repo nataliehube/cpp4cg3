0277d8a0808374533c0891bfc52e7fae57486980
Natalie Hube <nhube@stud.hs-bremen.de>
2014-05-07 16:25:57 +0200

    Aufgabe 3.1
    --- komplett

--- Uebung3/Uebung3/main.cpp
+++ Uebung3/Uebung3/main.cpp
@@ -1,20 +1,75 @@
 #include "stdafx.h"
 #include "GL/freeglut.h"
 
+
+static int wwidth = 640;
+static int wheight = 480;
+
+
+void renderScene(void) {
+	glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glutSwapBuffers();
+
+// Triangle
+	glBegin(GL_TRIANGLES);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glVertex3f(0.5, 0.0, 0.0);
+	glVertex3f(0.0, 0.5, 0.0);
+	glEnd();
+
+
+	// make it appear (before this, it's hidden in the rear buffer)
+	glFlush();
+	glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+
+	w = wwidth / 2;
+	h = wheight / 2;
+	
+	// Reset Matrix
+	glLoadIdentity();
+
+	// Set the viewport to be the entire window
+	glViewport(0, 0, w, h);
+
+	// Get Back to the Modelview
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y)
+{
+	if (key == 'y'){
+		exit(0);
+	}
+
+}
+
+//init GLUT create Window
 int main(int argc, char* argv[]) {
 	glutInit(&argc, argv);
-	return 0;
-}
+	// Note: glutSetOption is only available with freeglut
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,
+		GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(wwidth, wheight);
+	glutCreateWindow("cpp4g3-Window");
+
+	// register CALLBACKS --------------------------
+	glutDisplayFunc(renderScene);
+	//Window resize
+	glutReshapeFunc(reshape);
+
+	//----------------------------------------------
+
+	glutKeyboardFunc(keyboard);
+
+	// enter GLUT event processing cycle
+	glutMainLoop();
 
 
-//// Uebung3.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
-////
-//
-//#include "stdafx.h"
-//
-//
-//int _tmain(int argc, _TCHAR* argv[])
-//{
-//	return 0;
-//}
-//
+	return 1;
+}
