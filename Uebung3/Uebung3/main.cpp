284582378f4740903ba2b6710c540133aa8f704b
Natalie Hube <nhube@stud.hs-bremen.de>
2014-05-07 15:14:05 +0200

    first

---
+++ Uebung3/Uebung3/main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "GL/freeglut.h"
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	return 0;
+}
+
+
+//// Uebung3.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+////
+//
+//#include "stdafx.h"
+//
+//
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+//
